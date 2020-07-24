684e4916ff9798bf387177825fd6e2818d976390
Alex Vieira <alexvieiracb20@gmail.com>
2020-07-24 20:47:33 -0300

    Concluido desafio 3-2

---
+++ challenges-03/data.js
@@ -0,0 +1,24 @@
+module.exports = [
+    {
+        id: "starter",
+        image: "images/Starter.64c237cc.svg",
+        title: "Starter",
+        description: "5 módulos",
+        price: "Free"
+    },
+    {
+        id: "launchbase",
+        image: "images/LaunchBase.08285320.svg",
+        title: "LaunchBase",
+        description: "16 módulos",
+        price: "Paid"
+    },
+    {
+        id: "gostack",
+        image: "images/GoStack.83a178a0.svg",
+        title: "GoStackter",
+        description: "20 módulos",
+        price: "Paid"
+    },
+   
+]
--- challenges-03/server.js
+++ challenges-03/server.js
@@ -2,21 +2,40 @@ const express = require('express'); //chama o express
 const nunjucks = require('nunjucks');
 
 const server = express();
+const courses = require("./data");
 
 server.use(express.static('public'));
 
 server.set("view engine", "njk");
 
 nunjucks.configure("views", {
-    express: server
+    express: server,
+    autoescape: false,  //insere o html que está comentado
+    noCache: true
 });
 
 server.get("/", function(req, res){
-    return res.render("courses");
+    return res.render("courses", { items: courses });
 }); //pega a barra e executa a função
 
 server.get("/about", function(req, res){
-    return res.render("about");
+
+    const about = {
+        image: "https://avatars0.githubusercontent.com/u/28929274?s=200&v=4",
+        name: "Rocketseat",
+        description: 'Empresa focada em programação',
+        technologies: [
+            { name: "Node.js" },
+            { name: "React JS"},
+            { name: "React Native"}
+        ],
+        links: [
+            { name: "GitHub", url: "https://github.com/Rocketseat" },
+            { name: "Instagram", url: "https://www.linkedin.com/school/rocketseat/?originalSubdomain=br" },
+            { name: "Facebook", url: "https://www.facebook.com/rocketseat/" }
+        ]
+    }
+    return res.render("about", { about });
 }); //pega a barra e executa a função
 
 server.use(function(req, res) {
